6e3f17a140300439323d4e042061960e5b590297
Oboth03 <joboboth601@gmail.com>
2026-07-11 22:39:42 +0300

    my first commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // Set a string for the base path (must be quoted). Use '/' for development
+  // or '/react-todo-app/' if deploying to a subpath.
+  base: '/',
+});
